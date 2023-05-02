a5fa0554372ef316a9779ccd4352b28da59dd79a
Kareem <kareemelnaghy@aucegypt.edu>
2023-05-02 18:49:23 +0300

    Initial Commit

---
+++ Session 09/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Session 09
+//
+//  Created by Kareem Elnaghy on 5/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
